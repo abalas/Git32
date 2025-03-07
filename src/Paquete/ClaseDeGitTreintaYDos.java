3e8685ee48f05d49b1345be1ff6cdbbdb816be7d
alexa <alexa@DESKTOP-E9NFGID>
2025-03-08 00:08:23 +0100

    Modificacion del segundo usuario

--- src/Paquete/ClaseDeGitTreintaYDos.java
+++ src/Paquete/ClaseDeGitTreintaYDos.java
@@ -3,6 +3,7 @@ package Paquete;
 public class ClaseDeGitTreintaYDos {
 	public static void main(String[] args) {
 		System.out.println("Git 32");
+		System.out.println("Git 32 Actualizado");
 	}
 
 }
--- src/Paquete/TengoSueño.java
+++ src/Paquete/TengoSueño.java
@@ -3,6 +3,7 @@ package Paquete;
 public class TengoSueño {
 	public static void main(String[] args) {
 		System.out.println("Tengo sueño");
+		System.out.println("Sueño en latin es somnus");
 	}
 
 }
