2da8ac81b560697205dd0f036b7c7256d3b967cb
abalas <186952999+abalas@users.noreply.github.com>
2025-03-08 00:20:37 +0100

    Modificacion de los sysos de ambas las dos clases que habia para poner que me voy a ormir, como se pone sueño en rumano

--- src/Paquete/ClaseDeGitTreintaYDos.java
+++ src/Paquete/ClaseDeGitTreintaYDos.java
@@ -3,7 +3,7 @@ package Paquete;
 public class ClaseDeGitTreintaYDos {
 	public static void main(String[] args) {
 		System.out.println("Git 32");
-		System.out.println("Git 32 Actualizado");
+		System.out.println("Git 32 Actualizado 2.0");
 	}
 
 }
