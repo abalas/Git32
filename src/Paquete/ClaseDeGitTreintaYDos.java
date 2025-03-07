101a5aab088d71c1a11406b883bf02b3606841c7
abalas <186952999+abalas@users.noreply.github.com>
2025-03-07 23:49:49 +0100

    Primer commmit - practica Entornos Git32

---
+++ src/Paquete/ClaseDeGitTreintaYDos.java
@@ -0,0 +1,8 @@
+package Paquete;
+
+public class ClaseDeGitTreintaYDos {
+	public static void main(String[] args) {
+		System.out.println("Git 32");
+	}
+
+}
---
+++ src/Paquete/TengoSueño.java
@@ -0,0 +1,8 @@
+package Paquete;
+
+public class TengoSueño {
+	public static void main(String[] args) {
+		System.out.println("Tengo sueño");
+	}
+
+}
